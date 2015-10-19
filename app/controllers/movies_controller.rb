c2ec4209325347c7613da68e46829cbe5a363a14
Zamith <zamith.28@gmail.com>
2015-10-20 00:39:58 +0100

    Wrap movie info from the API into a model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,14 @@
 class MoviesController < ApplicationController
-
   def index
     @now_playing = Tmdb::Movie.now_playing
     if params[:search]
       @results = Tmdb::Movie.find(params[:search])
-    else
     end
   end
 
-  def show 
-    @movie = Tmdb::Movie.detail(params[:id])
+  def show
+    @movie = Movie.new(Tmdb::Movie.detail(params[:id]))
     @shout = Shout.new
     @shouts = Shout.where(movie_id: params[:id])
   end
-
 end
