dfb237ecd3583326119dc2d6336eb54d7fbe449d
Bruno Brito <brunofbrito@gmail.com>
2015-11-25 21:45:50 +0000

    Fixes crashes for users that are not signed in

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,12 @@ class MoviesController < ApplicationController
     if params[:search]
       @results = Tmdb::Movie.find(params[:search])
     end
-    followees_ids = current_user.followees(User).map(&:id)
-        #get only the ids of the people current_user folllows
-        followees_ids << current_user.id
-    @personal_activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User").order('created_at DESC').limit(20)
+    if current_user
+      followees_ids = current_user.followees(User).map(&:id) 
+      #get only the ids of the people current_user folllows
+      followees_ids << current_user.id
+      @personal_activities = PublicActivity::Activity.where(owner_id: followees_ids, owner_type: "User").order('created_at DESC').limit(20)
+    end
     @world_activities = PublicActivity::Activity.order('created_at DESC').limit(20)
   end
 
@@ -22,7 +24,9 @@ class MoviesController < ApplicationController
     @movie = Movie.new(Tmdb::Movie.detail(params[:id]))
     @cast = Tmdb::Movie.casts(@movie.id).map {|actor| actor["name"] }
     @shout = Shout.new
-    @own_shout = Shout.where(movie_id: @movie.id, user_id: current_user.id)
+    if current_user
+      @own_shout = Shout.where(movie_id: @movie.id, user_id: current_user.id)
+    end
     @shouts = Shout.where(movie_id: params[:id])
     url = "http://api.traileraddict.com/?imdb=#{@movie.imdb_id.delete("^0-9")}&count=4&width=000"
     doc = Nokogiri::HTML(open(url))
